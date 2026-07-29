69fed5d81537829e32abeb40b518c36cd6374af3
ApoorvaDeshpande-10101 <deshpandeapurva76@gmail.com>
2026-07-29 21:54:57 +0530

    Solve CheckPalindromeNumber

---
+++ 01_Learn_the_Basics/04_For_While_Loops/CheckPalindromeNumber.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+public class CheckPalindromeNumber {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter a numer: ");
+        int num = sc.nextInt();
+
+        int original = num;
+        int rev = 0;
+
+        while(num != 0){
+            int rem = num % 10;
+            rev = rev * 10 + rem;
+            num = num/10;
+        }
+
+        if(original == rev)
+            System.out.println(original+" is a Palindrome Number");
+        else
+            System.out.println(original+" is not a Palindrome Number");
+    }
+}
