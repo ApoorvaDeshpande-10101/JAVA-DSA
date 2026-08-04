b0364f1e2048a0adf2cca6aeb154618a2b27556f
ApoorvaDeshpande-10101 <deshpandeapurva76@gmail.com>
2026-08-04 23:48:47 +0530

    MaximumElement

---
+++ 01_Learn_the_Basics/06_Arrays/02_Functions_with_arrays/MaximumElement.java
@@ -0,0 +1,23 @@
+public class MaximumElement {
+
+    static int max(int[] arr) {
+
+        int max = arr[0];
+
+        for (int i = 1; i < arr.length; i++) {
+
+            if (arr[i] > max) {
+                max = arr[i];
+            }
+        }
+
+        return max;
+    }
+
+    public static void main(String[] args) {
+
+        int[] arr = {15, 28, 10, 45, 32};
+
+        System.out.println("Maximum Element = " + max(arr));
+    }
+}
