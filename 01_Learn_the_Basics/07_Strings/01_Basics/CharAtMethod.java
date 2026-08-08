aaa812b021f76023fdf350162b807a74cb3aa2aa
ApoorvaDeshpande-10101 <deshpandeapurva76@gmail.com>
2026-08-08 22:51:53 +0530

    String charAt Method

---
+++ 01_Learn_the_Basics/07_Strings/01_Basics/CharAtMethod.java
@@ -0,0 +1,10 @@
+public class CharAtMethod {
+    public static void main(String[] args) {
+
+        String name = "Apurva";
+
+        System.out.println(name.charAt(0));
+        System.out.println(name.charAt(1));
+        System.out.println(name.charAt(2));
+    }
+}
