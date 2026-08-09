ee0e28f1bb4df56ad91a0412ec64b25db9e22d43
ApoorvaDeshpande-10101 <deshpandeapurva76@gmail.com>
2026-08-09 22:58:23 +0530

    IndexOf Method

---
+++ 01_Learn_the_Basics/07_Strings/02_String_Operations/IndexOf.java
@@ -0,0 +1,9 @@
+public class IndexOf {
+    public static void main(String[] args) {
+
+        String name = "Apurva";
+
+        System.out.println(name.indexOf('a'));
+        System.out.println(name.indexOf('u'));
+    }
+}
