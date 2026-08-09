5ffdb4f7594e742d4c8e515ed4e6e7f43befe6dd
ApoorvaDeshpande-10101 <deshpandeapurva76@gmail.com>
2026-08-09 23:00:48 +0530

    String replace Method

---
+++ 01_Learn_the_Basics/07_Strings/02_String_Operations/Replace.java
@@ -0,0 +1,10 @@
+public class Replace {
+    public static void main(String[] args) {
+
+        String name = "Apurva";
+
+        String result = name.replace('a', 'x');
+
+        System.out.println(result);
+    }
+}
