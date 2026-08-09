fffde03d0ab9886b6d2eeffcf0906f86e442dfdb
ApoorvaDeshpande-10101 <deshpandeapurva76@gmail.com>
2026-08-09 22:57:16 +0530

    SubString Method

---
+++ 01_Learn_the_Basics/07_Strings/02_String_Operations/Substring.java
@@ -0,0 +1,9 @@
+public class Substring {
+    public static void main(String[] args) {
+
+        String name = "Apurva Deshpande";
+
+        System.out.println(name.substring(0, 6));
+        System.out.println(name.substring(7));
+    }
+}
