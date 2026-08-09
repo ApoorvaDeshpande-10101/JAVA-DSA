247b9d3ab4042c0bd6c8deb0c9e1b1f481aabe2d
ApoorvaDeshpande-10101 <deshpandeapurva76@gmail.com>
2026-08-09 22:59:36 +0530

    String contains Method

---
+++ 01_Learn_the_Basics/07_Strings/02_String_Operations/Contains.java
@@ -0,0 +1,9 @@
+public class Contains {
+    public static void main(String[] args) {
+
+        String name = "Apurva Deshpande";
+
+        System.out.println(name.contains("Apurva"));
+        System.out.println(name.contains("xyz"));
+    }
+}
