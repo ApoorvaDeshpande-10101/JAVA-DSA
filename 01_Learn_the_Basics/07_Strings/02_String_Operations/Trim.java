1a2eb75b88473bd69e6c4b6fe537e45aa42e2d11
ApoorvaDeshpande-10101 <deshpandeapurva76@gmail.com>
2026-08-10 23:19:04 +0530

    String Trim

---
+++ 01_Learn_the_Basics/07_Strings/02_String_Operations/Trim.java
@@ -0,0 +1,9 @@
+public class Trim {
+    public static void main(String[] args) {
+
+        String name = "   Apurva   ";
+
+        System.out.println(name);
+        System.out.println(name.trim());
+    }
+}
