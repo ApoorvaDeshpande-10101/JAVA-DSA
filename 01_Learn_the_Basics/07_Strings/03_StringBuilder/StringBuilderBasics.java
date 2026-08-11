f4e0a47ddc309c2f26c7b0182f1eb630f0aa7980
ApoorvaDeshpande-10101 <deshpandeapurva76@gmail.com>
2026-08-11 23:57:26 +0530

    String Builder

---
+++ 01_Learn_the_Basics/07_Strings/03_StringBuilder/StringBuilderBasics.java
@@ -0,0 +1,8 @@
+public class StringBuilderBasics {
+    public static void main(String[] args) {
+
+        StringBuilder sb = new StringBuilder("Hello");
+
+        System.out.println(sb);
+    }
+}
