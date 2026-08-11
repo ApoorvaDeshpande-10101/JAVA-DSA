3674302330db722e1ae8de28942b67f6fa1f0108
ApoorvaDeshpande-10101 <deshpandeapurva76@gmail.com>
2026-08-12 00:00:11 +0530

    String Builder Insert

---
+++ 01_Learn_the_Basics/07_Strings/03_StringBuilder/StringBuilderInsert.java
@@ -0,0 +1,10 @@
+public class StringBuilderInsert {
+    public static void main(String[] args) {
+
+        StringBuilder sb = new StringBuilder("Hello World");
+
+        sb.insert(6, "Java ");
+
+        System.out.println(sb);
+    }
+}
