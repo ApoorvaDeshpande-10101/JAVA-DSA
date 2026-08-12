c3a9d748947c7dcea5ec6a8cb6cc6fa85de4ea1a
ApoorvaDeshpande-10101 <deshpandeapurva76@gmail.com>
2026-08-12 23:16:18 +0530

    String Builder Replace

---
+++ 01_Learn_the_Basics/07_Strings/03_StringBuilder/StringBuilderReplace.java
@@ -0,0 +1,10 @@
+public class StringBuilderReplace {
+    public static void main(String[] args) {
+
+        StringBuilder sb = new StringBuilder("Hello Java");
+
+        sb.replace(6, 10, "World");
+
+        System.out.println(sb);
+    }
+}
