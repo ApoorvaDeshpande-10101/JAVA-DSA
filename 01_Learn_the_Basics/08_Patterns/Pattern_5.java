8749d8cc121db9f1fc5f74d7d33b205526e5b575
ApoorvaDeshpande-10101 <deshpandeapurva76@gmail.com>
2026-08-14 23:29:56 +0530

    Pattern 5

---
+++ 01_Learn_the_Basics/08_Patterns/Pattern_5.java
@@ -0,0 +1,28 @@
+/*
+--------
+n=5
+*****
+****
+***
+**
+*
+--------
+*/
+
+import java.util.Scanner;
+public class Pattern_5 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter value of n: ");
+        int n = sc.nextInt();
+
+        for (int i = 0; i < n; i++) {
+            for (int j = i; j < n; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        sc.close();
+    }
+}
