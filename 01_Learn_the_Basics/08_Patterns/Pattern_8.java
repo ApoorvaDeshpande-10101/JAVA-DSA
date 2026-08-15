e4f18a5208871dbcfe4e1d8a046263bfd4c66ba9
ApoorvaDeshpande-10101 <deshpandeapurva76@gmail.com>
2026-08-15 23:01:53 +0530

    Pattern 8

---
+++ 01_Learn_the_Basics/08_Patterns/Pattern_8.java
@@ -0,0 +1,32 @@
+/*
+-----------
+n=5
+*********
+ *******
+  *****
+   ***
+    *
+-----------
+*/
+
+import java.util.Scanner;
+
+public class Pattern_8 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter value of n: ");
+        int n = sc.nextInt();
+
+        for (int i = 0; i < n; i++) {
+            for (int k = 0; k < i; k++) {
+                System.out.print(" ");
+            }
+            for (int j = 0; j < 2*(n-i)-1 ; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        sc.close();
+    }
+}
