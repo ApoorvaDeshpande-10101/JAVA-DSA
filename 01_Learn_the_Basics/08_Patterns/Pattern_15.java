45df01baeb5a9d3600e0ada9f2bb02980387597b
ApoorvaDeshpande-10101 <deshpandeapurva76@gmail.com>
2026-08-18 22:10:21 +0530

    Pattern 15

--- 01_Learn_the_Basics/08_Patterns/Pattern_15.java
+++ 01_Learn_the_Basics/08_Patterns/Pattern_15.java
@@ -1,24 +1,24 @@
 /*
 ----------
 n=5
-A
-AB
-ABC
-ABCD
 ABCDE
+ABCD
+ABC
+AB
+A
 ----------
 */
 
 import java.util.Scanner;
-public class Pattern_14 {
+public class Pattern_15 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
         System.out.print("Enter value of n: ");
         int n = sc.nextInt();
 
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j <= i; j++) {
+        for (int i = 1; i <= n; i++) {
+            for (int j = 0; j <= n-i; j++) {
                 char ch = (char) ('A' + j);
                 System.out.print(ch);
             }
