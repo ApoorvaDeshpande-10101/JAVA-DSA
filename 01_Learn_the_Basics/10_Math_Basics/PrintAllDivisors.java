d679ac97d8f2e8bdec29c89806e5f172bb249fbc
ApoorvaDeshpande-10101 <deshpandeapurva76@gmail.com>
2026-08-21 23:05:00 +0530

    Print All Divisors

---
+++ 01_Learn_the_Basics/10_Math_Basics/PrintAllDivisors.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class PrintAllDivisors {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a number: ");
+        int num = sc.nextInt();
+
+        System.out.println("Divisors of "+num+": ");
+
+        for (int i = 1; i * i <= num; i++) {
+            if(num % i == 0){
+                System.out.print(i+" ");
+
+                if (i != num / i) {
+                    System.out.print(num / i + " ");
+                }
+            }
+        }
+        sc.close();
+    }
+}
